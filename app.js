2809fb1ecc49fdad5831d2d266c7ab79e43fd426
Felipe Silveira <felipesilveira@MacBook-Pro-de-Felipe.local>
2022-05-17 15:34:12 -0300

    criando javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+// Book Class: Represents a Book
+
+class Book {
+    constructor(title, author, isbn){
+        
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
+
+//
